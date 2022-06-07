f7bf620de3b06b01e3a5d1cd12d5357c15195f4f
Analia Perez <analianoemip@hotmail.com>
2022-06-06 21:13:34 -0300

    agregar función ings2i

---
+++ ings2i.py
@@ -0,0 +1,5 @@
+def ings2i():
+    param1 = int(input("introduzca un número entero: "))
+    param2 = int(input("introduzca un segundo número entero: "))
+    listaDeEnteros = [param1, param2]
+    return listaDeEnteros
