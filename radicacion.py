be670d84edeeb6ff31b0eed845cdee23a23b22b2
Mar Octtinger <oceantothemoon@gmail.com>
2022-06-07 10:38:40 -0300

    Arreglos de retorno funcion radicacion

--- radicacion.py
+++ radicacion.py
@@ -1,18 +1,19 @@
 def radicacion(a, b):
-    # Comprobar que el índice no sea 0
-    if b == 0:
-        print('Error, el índice de una raiz no puede ser 0')
-        return
+    try:
+        b = 1 / b
+        # Comprobar si el radicando es negativo y el índice par
+        if a < 0 and b % 2 == 0:
+            return 'No existe la raiz'
+        elif a < 0 and b % 2 != 0:
+            a = a * -1
+            resultado = - (a ** b)
+        else:
+            resultado = (a ** b)
 
-    # Comprobar si el radicando es negativo y el índice par
-    if a < 0 and b % 2 == 0:
-        print('No existe la raiz')
-        return
+        return round(resultado, 4)
 
-    if a < 0 and b % 2 != 0:
-        a = a * (-1)
-        resultado = - (a ** (1/b))
-    else:
-        resultado = (a ** (1/b))
-
-    return round(resultado, 4)
+    # Excepción para índice no sea 0
+    except ZeroDivisionError:
+        return 'Error, el índice de una raiz no puede ser 0'
+    except Exception as e:
+        return e
