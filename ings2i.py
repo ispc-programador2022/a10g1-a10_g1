6a8722c99a56cc704ee164586468cc68d1bcd9b2
Analía Perez <48915108+AnaliaPerez@users.noreply.github.com>
2022-06-06 21:33:57 -0300

    Añadir el print

--- ings2i.py
+++ ings2i.py
@@ -2,4 +2,5 @@ def ings2i():
     param1 = int(input("introduzca un número entero: "))
     param2 = int(input("introduzca un segundo número entero: "))
     listaDeEnteros = [param1, param2]
+    print(listaDeEnteros)
     return listaDeEnteros
